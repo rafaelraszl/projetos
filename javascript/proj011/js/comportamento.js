c49e83e6987f57dcb88d160db56da0481446a2dc
Rafael Raszl <rafaelmsraszl@gmail.com>
2020-07-28 23:18:58 -0300

    Melhorias  js ===> limpar campos após voltar click

--- javascript/proj011/js/comportamento.js
+++ javascript/proj011/js/comportamento.js
@@ -13,21 +13,69 @@ function mostrarBotaoIMC() {
     $('.resultado').classList.add('escondido');
 }
 
-$('.botaoIMC').addEventListener('click', function() {
-    calcularResultado();
-    mostrarResultado();
-}); 
+$('.botaoIMC').addEventListener('click', () => {
+    const altura = $('#altura');
+    const peso = $('#peso');
 
-$('.voltar').addEventListener('click', function() {
-    mostrarBotaoIMC();
+    if (altura.value == '') {
+        altura.focus();
+    }
+    else if (peso.value == '') {
+        peso.focus();
+    }
+    else {
+        calcularResultado(altura, peso);
+        mostrarResultado();
+    }
 });
 
-function calcularResultado(){
-    const imc = calcularIMC($('#altura').value, $('#peso').value);
+$('.voltar').addEventListener('click', () => {
+    mostrarBotaoIMC();
+    clearInputUrlNumberText('altura');
+    clearInputUrlNumberText('peso');
+
+
+});
+
+function calcularResultado(altura, peso){
+    const imc = calcularIMC(altura.value, peso.value);
     $('.imcValor').innerHTML = imc.toLocaleString('pt-BR', { maximumFractionDigits: 2 });
-    $('.imcDesc').innerHTML = 'Saudável';
+    $('.imcDesc').innerHTML = descricaoIMC(imc);
 }
 
 function calcularIMC(altura, peso) {
     return peso / (altura * altura);
 }
+
+function descricaoIMC(imc) {
+    if (imc < 16) {
+        return 'Magreza grave';
+    }
+    else if (imc >=16 && imc <17) {
+        return 'Magreza moderada';
+    }
+    else if (imc >=17 && imc < 18.5) {
+        return 'Magreza leve';
+    }
+    else if (imc >= 18.5 && imc < 25) {
+        return 'Saudável';
+      }
+      else if (imc >= 25 && imc < 30) {
+        return 'Sobrepeso';
+      }
+      else if (imc >= 30 && imc < 35) {
+        return 'Obesidade';
+      }
+      else if (imc >= 35 && imc < 40) {
+        return 'Obesidade Severa';
+      }
+      else {
+        return 'Obesidade Mórbida';
+      }
+}
+
+function clearInputUrlNumberText(name) {
+    var campos = document.querySelectorAll("input[name='"+name+"']");
+    [].map.call(campos, altura => altura.value = '');
+    [].map.call(campos, peso => peso.value = '');
+  }
